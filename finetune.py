827e8d40aaeef37e7c56ae16c7cbc4de3afe4cc8
Leon Zong <zongleon03@gmail.com>
2025-08-27 15:07:59 -0400

    add pop prediction task

--- finetune.py
+++ finetune.py
@@ -2,24 +2,49 @@ import torch
 from models import HapbertaForSequenceClassification
 from transformers import TrainingArguments, Trainer
 from datasets import load_from_disk
-from sklearn.metrics import accuracy_score, f1_score, mean_absolute_error, mean_squared_error, r2_score
+from sklearn.metrics import accuracy_score, f1_score, mean_absolute_error, mean_squared_error, r2_score, confusion_matrix
 from collators import HaploSimpleNormalDataCollator
 
-# MODE = "realsim"
-MODE = "sel"
-MODE = "sel2"
+MODE = "pop"
 
 if MODE == "realsim":
     dataset_path = "dataset/tokenizedrealsim"
     output_path = "./models/hapberta2d_realsim"
+    num_labels = 2
+    typ = torch.long
 elif MODE == "sel":
     dataset_path = "dataset/tokenizedsel"
     output_path = "./models/hapberta2d_sel"
-else:
+    num_labels = 1 # continuous
+    typ = torch.float32
+elif MODE == "sel2":
     dataset_path = "dataset/tokenizedsel2"
     output_path = "./models/hapberta2d_sel_binary"
+    num_labels = 2
+    typ = torch.long
+elif MODE == "pop":
+    dataset_path = "dataset/tokenized"
+    output_path = "./models/hapberta2d_pop"
+    num_labels = 3
+    typ = torch.long
+else:
+    raise ValueError("Incorrect mode selected")
 
 dataset = load_from_disk(dataset_path)
+
+if MODE == "pop":
+    # dataset = dataset.shuffle().select(range(10000))
+
+    lbl2id = {
+        "CEU": 0,
+        "CHB": 1,
+        "YRI": 2,
+    }
+    def process_pop(example):
+        example["label"] = lbl2id[example["pop"]]
+        return example
+
+    dataset = dataset.map(process_pop)
 
 # Split dataset
 dataset = dataset.train_test_split(test_size=0.1)
@@ -28,25 +53,24 @@ eval_dataset = dataset["test"]
 
 model = HapbertaForSequenceClassification.from_pretrained("./models/hapberta2d",
                                                             classifier_dropout=0,
-                                                            num_labels=1 if MODE == "sel" else 2,
-                                                            )
+                                                            num_labels=num_labels,
+)
 
-collator = HaploSimpleNormalDataCollator(label_dtype=torch.float32 if MODE == "sel" else torch.long)
+collator = HaploSimpleNormalDataCollator(label_dtype=typ)
 
 # training arguments
 training_args = TrainingArguments(
     output_dir=output_path,
     overwrite_output_dir=True,
-    num_train_epochs=20,
+    num_train_epochs=10,
     per_device_train_batch_size=32,
     per_device_eval_batch_size=32,
->>>>>>> f4ed8db (parcc training updates)
     warmup_ratio=0.1,
     weight_decay=0.01,
     logging_dir="./logs",
     logging_steps=100,
     save_steps=500,
-    eval_steps=500,
+    eval_steps=100,
     eval_strategy="steps",
     save_strategy="steps",
     save_total_limit=2,
@@ -64,9 +88,15 @@ def compute_metrics(eval_pred):
     logits, labels = eval_pred
     if MODE != "sel":
         preds = logits.argmax(axis=-1)
-        acc = accuracy_score(labels, preds)
-        f1 = f1_score(labels, preds)
-        return {"accuracy": acc, "f1": f1}
+        if MODE == "pop":
+            acc = accuracy_score(labels, preds)
+            f1 = f1_score(labels, preds, average="micro")
+            matr = confusion_matrix(labels, preds)
+            return {"accuracy": acc, "f1": f1, "matrix": matr.ravel().tolist()}
+        else:
+            acc = accuracy_score(labels, preds)
+            f1 = f1_score(labels, preds)
+            return {"accuracy": acc, "f1": f1}
     else:
         mse = mean_squared_error(labels, logits)
         mae = mean_absolute_error(labels, logits)
